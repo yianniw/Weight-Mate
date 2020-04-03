6015c05485cf87d677c613b7d85e81f8c57d3265
Tan arin <tanarin12@gmail.com>
2020-04-02 21:03:01 -0400

    Created Landing and Login Page

---
+++ app/src/main/java/edu/fsu/cs/weightmate/LandingPage.java
@@ -0,0 +1,14 @@
+package edu.fsu.cs.weightmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_page);
+    }
+}
