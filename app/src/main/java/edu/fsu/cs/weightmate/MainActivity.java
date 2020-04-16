dcfe02ddfa5b4aec145d9d8a42788b91f6f44b58
Deathgibo <gvm16@my.fsu.edu>
2020-04-16 17:39:48 -0400

    fixed main back to normal

--- app/src/main/java/edu/fsu/cs/weightmate/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/weightmate/MainActivity.java
@@ -13,9 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_page);
-
-        Intent intent = new Intent(MainActivity.this, AddMealActivity.class);
-        startActivity(intent);
+        
         //startProfile();
     }
 
