73e1098452efc2074b41b09ba950591de9c7d062
Deathgibo <gvm16@my.fsu.edu>
2020-04-16 17:38:13 -0400

    got rid of items for meal
    changed page so its just meal name, carbs, calories, fat, protein. No items anymore.

--- app/src/main/java/edu/fsu/cs/weightmate/AddMealActivity.java
+++ app/src/main/java/edu/fsu/cs/weightmate/AddMealActivity.java
@@ -15,6 +15,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,274 +23,71 @@ import java.util.List;
 
 public class AddMealActivity extends AppCompatActivity {
 
-    public class ItemInfo{
-        public String itemname;
-        public double calories;
-        public double protein;
-        public double carbs;
-
-        public void setitemname(String a){
-            itemname = a;
-        }
-        public void setcalories(double a){
-            calories = a;
-        }
-        public void setprotein(double a){
-            protein = a;
-        }
-        public void setcarbs(double a){
-            carbs = a;
-        }
-    }
-
-    ArrayList<ItemInfo> iteminfo;
 
     EditText mealname;
-    EditText itemname;
+    EditText itemfat;
     EditText itemcalories;
     EditText itemprotein;
     EditText itemcarbs;
-    public boolean deleteinfo;
-
-    Spinner spinner;
-    ArrayAdapter<String> adapter;
-    List<String> items;
-
-    TextView totalcalories;
-    TextView totalprotein;
-    TextView totalcarbs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meal);
 
-        deleteinfo = false;
         //EditTexts
         mealname = findViewById(R.id.mealnametext);
         mealname.setText("Meal");
-        itemname = findViewById(R.id.itemnametext);
+        itemfat = findViewById(R.id.itemnametext);
         itemcalories = findViewById(R.id.itemcalorytext);
         itemprotein = findViewById(R.id.itemproteintext);
         itemcarbs = findViewById(R.id.itemcarbtext);
 
-        //TextViews
-        totalcalories = findViewById(R.id.totalcaloryview);
-        totalprotein = findViewById(R.id.totalproteinview);
-        totalcarbs = findViewById(R.id.totalcarbview);
-        totalcarbs.setText("0.0");
-        totalprotein.setText("0.0");
-        totalcalories.setText("0.0");
+        itemfat.setText("0.0");
+        itemcalories.setText("0.0");
+        itemprotein.setText("0.0");
+        itemcarbs.setText("0.0");
 
-        //Spinner
-        spinner= findViewById(R.id.spinner);
-        String[] names = new String[]{"New Item"};
-        items = new ArrayList<>(Arrays.asList(names));
-
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        spinner.setAdapter(adapter);
-        iteminfo = new ArrayList<ItemInfo>();
-        ItemInfo a = new ItemInfo();
-        a.itemname = "New Item";
-        a.calories = 0.0;
-        a.protein = 0.0;
-        a.carbs = 0.0;
-        iteminfo.add(a);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                Log.d("lol","item selected");
-
-                int index = spinner.getSelectedItemPosition();
-
-                itemname.setText(iteminfo.get(index).itemname);
-                itemcalories.setText(Double.toString(iteminfo.get(index).calories));
-                itemprotein.setText(Double.toString(iteminfo.get(index).protein));
-                itemcarbs.setText(Double.toString(iteminfo.get(index).carbs));
-            }
-
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                Log.d("lol","onNothingSelected");
-            }
-
-        });
-
-        itemname.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                int index = spinner.getSelectedItemPosition();
-                iteminfo.get(index).setitemname(itemname.getText().toString());
-
-                items.set(index,itemname.getText().toString());
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-                // TODO Auto-generated method stub
-            }
-        });
-
-        itemcalories.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                int index = spinner.getSelectedItemPosition();
-                if(itemcalories.getText().toString().isEmpty()){
-                    iteminfo.get(index).setcalories(0.0);
-                }
-                else{
-                    iteminfo.get(index).setcalories(Double.parseDouble(itemcalories.getText().toString()));
-                }
-
-                totalcalories.setText(Double.toString(calculatetotalcalories()));
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-                // TODO Auto-generated method stub
-            }
-        });
-
-        itemprotein.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                int index = spinner.getSelectedItemPosition();
-                if(itemprotein.getText().toString().isEmpty()){
-                    iteminfo.get(index).setprotein(0.0);
-                }
-                else{
-                    iteminfo.get(index).setprotein(Double.parseDouble(itemprotein.getText().toString()));
-                }
-
-                totalprotein.setText(Double.toString(calculatetotalprotein()));
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-                // TODO Auto-generated method stub
-            }
-        });
-
-        itemcarbs.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                int index = spinner.getSelectedItemPosition();
-                if(itemcarbs.getText().toString().isEmpty()){
-                    iteminfo.get(index).setcarbs(0.0);
-                }
-                else{
-                    iteminfo.get(index).setcarbs(Double.parseDouble(itemcarbs.getText().toString()));
-                }
-
-                totalcarbs.setText(Double.toString(calculatetotalcarbs()));
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-                // TODO Auto-generated method stub
-            }
-        });
-    }
-
-    //switch spinner
-
-    public double calculatetotalcalories(){
-        double counter = 0.0;
-        for(int i = 0;i<iteminfo.size();i++){
-            counter += iteminfo.get(i).calories;
-        }
-        return counter;
-    }
-    public double calculatetotalprotein(){
-        double counter = 0.0;
-        for(int i = 0;i<iteminfo.size();i++){
-            counter += iteminfo.get(i).protein;
-        }
-        return counter;
-    }
-    public double calculatetotalcarbs(){
-        double counter = 0.0;
-        for(int i = 0;i<iteminfo.size();i++){
-            counter += iteminfo.get(i).carbs;
-        }
-        return counter;
-    }
-
-    public void addbtn(View v) {
-        Log.d("dbd","addbtn");
-        items.add("New Item");
-        adapter.notifyDataSetChanged();
-
-        ItemInfo a = new ItemInfo();
-        a.itemname = "New Item";
-        a.calories = 0.0;
-        a.protein = 0.0;
-        a.carbs = 0.0;
-        iteminfo.add(a);
-        //adapter.notifyDataSetChanged();
-        //spinner.setAdapter(adapter);
-    }
-
-    public void deletebtn(View v){
-        Log.d("dbd","deletebtn");
-        if(items.size() > 1){
-            items.remove(spinner.getSelectedItemPosition());
-            iteminfo.remove(spinner.getSelectedItemPosition());
-            int index = spinner.getSelectedItemPosition();
-            adapter.notifyDataSetChanged();
-            if(items.size() == spinner.getSelectedItemPosition()){
-                return;
-            }
-
-            itemname.setText(iteminfo.get(index).itemname);
-            itemcalories.setText(Double.toString(iteminfo.get(index).calories));
-            itemprotein.setText(Double.toString(iteminfo.get(index).protein));
-            itemcarbs.setText(Double.toString(iteminfo.get(index).carbs));
-        }
     }
 
     public void finishmealbtn(View v){
         Log.d("dbd","finishmealbtn");
 
+        //check if everything isnt empty
+        if(mealname.getText().toString().isEmpty()){
+            Toast toast = Toast.makeText(getApplicationContext(), "Enter meal name", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+        else if(itemcalories.getText().toString().isEmpty()){
+            Toast toast = Toast.makeText(getApplicationContext(), "Enter calory value", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+        else if(itemprotein.getText().toString().isEmpty()){
+            Toast toast = Toast.makeText(getApplicationContext(), "Enter protein value", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+        else if(itemfat.getText().toString().isEmpty()){
+            Toast toast = Toast.makeText(getApplicationContext(), "Enter fat value", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+        else if(itemcarbs.getText().toString().isEmpty()){
+            Toast toast = Toast.makeText(getApplicationContext(), "Enter carb value", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+
         ContentValues values = new ContentValues();
-        //employee id, name, email, gender, passwd, department
         values.put("username","Toby");
         values.put("day","April 1");
         values.put("mealname", mealname.getText().toString());
-        values.put("calories",Double.parseDouble(totalcalories.getText().toString()));
-        values.put("protein",Double.parseDouble(totalprotein.getText().toString()));
-        values.put("carbs",Double.parseDouble(totalcarbs.getText().toString()));
-        values.put("fat",0.0);
+        values.put("calories",Double.parseDouble(itemcalories.getText().toString()));
+        values.put("protein",Double.parseDouble(itemprotein.getText().toString()));
+        values.put("carbs",Double.parseDouble(itemcarbs.getText().toString()));
+        values.put("fat",Double.parseDouble(itemfat.getText().toString()));
 
         Uri inserturi = getContentResolver().insert(MyContentProvider.CONTENT_URI_MEAL,values);
 
