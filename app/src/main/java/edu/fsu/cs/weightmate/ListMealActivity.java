3652a1c143866e5569f07edcbbb9616376254541
Cameron Heffelfinger <cjh17h@my.fsu.edu>
2020-04-16 20:23:41 -0400

    Added a scrollable listview on the listMealActivity. Currently displays placeholder values until the meal log content provider is completed. Also added a listmealentry layout that may or may not be used in the future.

--- app/src/main/java/edu/fsu/cs/weightmate/ListMealActivity.java
+++ app/src/main/java/edu/fsu/cs/weightmate/ListMealActivity.java
@@ -3,7 +3,11 @@ package edu.fsu.cs.weightmate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ListMealActivity extends AppCompatActivity {
 
@@ -16,6 +20,8 @@ public class ListMealActivity extends AppCompatActivity {
     TextView remainingProtein;
     TextView remainingCarbs;
     TextView remainingFat;
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +46,18 @@ public class ListMealActivity extends AppCompatActivity {
         //User Information, for now we will use placeholders until other components are
         //setup
 
+        listView = (ListView)findViewById(R.id.listMealListView);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Breakfast");
+        arrayList.add("100 Calories");
+        arrayList.add("5g Protein");
+        arrayList.add("2g Carbs");
+        arrayList.add("3g Fat");
+
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
 
 
     }
